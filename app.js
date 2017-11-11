91d961d0a043e6a63fa480eb733ce483cecddc13
Jan Dix <dix.jan@gmx.de>
2017-11-11 18:52:31 +0100

    add twitter minimal example

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const handlebars = require('express-handlebars')
     .create({defaultLayout: 'main'});
-
+const Twitter = require('twitter');
 
 var app = express();
 
@@ -11,6 +11,27 @@ app.set('view engine', 'handlebars');
 app.set('port', 8080);
 
 
+
+var client = new Twitter({
+    consumer_key: 'W6HSyXNXsWk9rq3SjNAtlBZuO',
+    consumer_secret: 'mGG5ezQ6cNM6wfXQj1w9DGQV5lJzQLVL8Tf1CFpbb31ZQkK4Rv',
+    access_token_key: '3372623884-iqa73dotaZgHB7q8twAfIvxHgf0CC211fQcDmKg',
+    access_token_secret: 'RZiuaiUPe3U0R7qVsSFAeKQpwV5CNohNFeFCGTsNGlYPz'
+});
+
+/**
+ * Stream statuses filtered by keyword
+ * number of tweets per second depends on topic popularity
+ **/
+client.stream('statuses/filter', {track: '#rstats'},  function(stream) {
+    stream.on('data', function(tweet) {
+        console.log(tweet.text);
+    });
+
+    stream.on('error', function(error) {
+        console.log(error);
+    });
+});
 
 
 // wall
